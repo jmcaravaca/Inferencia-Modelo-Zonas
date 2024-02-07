8d69758d4a3639b028be9953f39a2e4b038f310b
jmcaravaca <jmcaravaca@minsait.com>
2024-02-07 16:03:59 +0100

    Probado, pendiente de refactorizar

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 
 import json
 from yoloinference import infer
+import cv2
+import uuid
+import os
 
 class Main(object):
    def __init__(self):
-      pass
+      weightsPath = os.path.join(os.getcwd(), "zonas",  "zonas.weights")
+      configPath = os.path.join(os.getcwd(), "zonas" ,  "zonas.cfg")
+      self.yolonet = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+      self.yolonet.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+      self.yolonet.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
    def predict(self, skill_input):
       import io
       file_like = io.BytesIO(skill_input)
+      # Write img to file
       with file_like as f:
-         filepath = os.path.join("img", "tempfile.png")
-         f.write(filepath)
-      results = infer.infer_from_text(text_data)
-      return json.dumps(results)
+         unique_id = uuid.uuid4()
+         filepath = os.path.join("img", f"tempfile_{unique_id}.png")
+         with open(filepath, 'wb') as output_file:
+            output_file.write(file_like.read())         
+      results = infer.infer_from_imgpath(filepath, self.yolonet)
+      os.remove(filepath)
+      return results
       
       
       
 if __name__ == '__main__':
    a = Main()
    import os
-   with open(os.path.join("testdata","message_474.txt")) as f:
-         text_data = f.read()
-   results = infer.infer_from_text(text_data)
-   print(results)
+   for filename in os.listdir("testdata"):
+      try:
+         print(f"....{filename}.....")
+         with open(os.path.join("testdata", filename), 'rb') as f:
+               results = a.predict(skill_input=f.read())
+               print(results)
+      except Exception as e:
+         print(e)
--- yoloinference/infer.py
+++ yoloinference/infer.py
@@ -1,51 +1,39 @@
 # import the necessary packages
 from random import randrange
 import numpy as np
+import pickle
 import time
 import cv2
 import os
+import json
 
-direct_in = "img"
-out = "result"
-confianza = 0.5
-umbral = 0.1
-weightsPath = os.path.join(os.getcwd(), "zonas",  "zonas.weights")
-configPath = os.path.join(os.getcwd(), "zonas" ,  "zonas.cfg")
-inference_width = 416
-inference_height = 608
 
-confianza = float(confianza)
-umbral = float(umbral)
+def infer_from_imgpath(imagen: str, net):
+    out_path = "result"
+    confianza = 0.5
+    umbral = 0.1
+    inference_width = 416
+    inference_height = 608
+    confianza = float(confianza)
+    umbral = float(umbral)
+    # load the COCO class labels our YOLO model was trained on
+    LABELS = ["0", "1", "2", "3", "4", "5"]
+    # BGR Format
+    COLORS = [[0,0,255],[255,0,255],[255,0,0],[0,255,0],[128,0,128]]
+    # load our YOLO object detector trained on COCO dataset (80 classes)
 
-# load the COCO class labels our YOLO model was trained on
-LABELS = ["0", "1", "2", "3", "4", "5"]
-
-# BGR Format
-COLORS = [[0,0,255],[255,0,255],[255,0,0],[0,255,0],[128,0,128]]
-
-# load our YOLO object detector trained on COCO dataset (80 classes)
-print("[INFO] loading YOLO from disk...")
-net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
-
-net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
-id_general = 1000
-
-valid_extensions = (".jpg", ".jpeg", ".png", ".tiff", ".tif")
-for imagen in os.listdir(direct_in):
+    valid_extensions = (".jpg", ".jpeg", ".png", ".tiff", ".tif")
     if not (imagen.endswith(".jpg") or imagen.endswith(".JPG") or imagen.endswith(".png") or imagen.endswith(".PNG") or imagen.endswith(".jpeg") or imagen.endswith(".tiff") or imagen.endswith(".tif")):
-        continue
+        raise ValueError("Extension not valid")
     # load our input image and grab its spatial dimensions
-    image = cv2.imread(os.path.join(direct_in, imagen))
+    image = cv2.imread(imagen)
     image_orig = image
     image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
     image = np.stack((image,)*3, axis=-1)
     (H, W) = image.shape[:2]
-
     # determine only the *output* layer names that we need from YOLO
     ln = net.getLayerNames()
     ln = [ln[i - 1] for i in net.getUnconnectedOutLayers()]
-
     # construct a blob from the input image and then perform a forward
     # pass of the YOLO object detector, giving us our bounding boxes and
     # associated probabilities
@@ -55,16 +43,13 @@ for imagen in os.listdir(direct_in):
     net.setInput(blob)
     layerOutputs = net.forward(ln)
     end = time.time()
-
     # show timing information on YOLO
     print("[INFO] YOLO took {:.6f} seconds".format(end - start))
-
     # initialize our lists of detected bounding boxes, confidences, and
     # class IDs, respectively
     boxes = []
     confidences = []
     classIDs = []
-
     # loop over each of the layer outputs
     for output in layerOutputs:
         # loop over each of the detections
@@ -74,7 +59,6 @@ for imagen in os.listdir(direct_in):
             scores = detection[5:]
             classID = np.argmax(scores)
             confidence = scores[classID]
-
             # filter out weak predictions by ensuring the detected
             # probability is greater than the minimum probability
             if confidence > confianza:
@@ -84,26 +68,23 @@ for imagen in os.listdir(direct_in):
                 # box followed by the boxes' width and height
                 box = detection[0:4] * np.array([W, H, W, H])
                 (centerX, centerY, width, height) = box.astype("int")
-
                 # use the center (x, y)-coordinates to derive the top and
                 # and left corner of the bounding box
                 x = int(centerX - (width / 2))
                 y = int(centerY - (height / 2))
-
                 # update our list of bounding box coordinates, confidences,
                 # and class IDs
                 boxes.append([x, y, int(width), int(height)])
                 confidences.append(float(confidence))
                 classIDs.append(classID)
-
     # apply non-maxima suppression to suppress weak, overlapping bounding
     # boxes
     idxs = cv2.dnn.NMSBoxes(boxes, confidences, confianza, umbral)
-
     # ensure at least one detection exists
     anchos = []
     altos = []
     if len(idxs) > 0:
+        results = {}
         # loop over the indexes we are keeping
         for i in idxs.flatten():
             # extract the bounding box coordinates
@@ -117,19 +98,23 @@ for imagen in os.listdir(direct_in):
                 y = 0
             anchos.append(w)
             altos.append(h)
-            
+            results[int(i)] = { "box" : (x, y, w, h),
+                          "clase" : clase_str[0],
+                          "confidence" : confidences[i],
+                          "boxformat": "xywh"}
             # draw a bounding box rectangle and label on the image
             color = [int(c) for c in COLORS[classIDs[i]]]
             # ###################################
             # # IMAGEN
             # ###################################            
             cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
-            
             text = "{}".format(clase_str[0])
             cv2.putText(image, text, (x-10, y + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
             cv2.putText(image,str(cajaConfianza),(x, y + 10),cv2.FONT_HERSHEY_SIMPLEX ,0.5 ,(0,120,0) ,thickness=2)
-            
-    if not os.path.exists(out):
-        os.makedirs(out)
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
+    cv2.imwrite(os.path.join(out_path, os.path.basename(imagen)), image)
+    #serialized = pickle.dumps(idxs).decode('latin-1')
+    return json.dumps(results)
 
-    cv2.imwrite(os.path.join(out, imagen), image)
+
